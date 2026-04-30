fb9445eca3cc74060b150e114fef34afd750f192
AlexandrSamoylenko <heylalalay@mail.ru>
2026-04-30 15:49:07 +0400

    feat: create promo mobile

--- src/init/sections/ui/mobile/PromoMobile.vue
+++ src/init/sections/ui/mobile/PromoMobile.vue
@@ -5,25 +5,25 @@
       WITH CLOTHING&nbsp;BRANDS.
     </h2>
     <div class="grid grid-cols-2">
-      <img class="w-5/6" :src="promoSectionImg1" alt="" />
+      <div>
+        <img class="mb-18 w-5/6" :src="promoSectionImg1" alt="" />
+        <div class="pl-4 text-sm">
+          <p>BEAUTY IS IN THE EYE OF&nbsp;THE BEHOLDER</p>
+        </div>
+      </div>
       <div class="pt-4 flex flex-col gap-y-8">
         <div class="pr-6 flex justify-start">
           <p class="pt-1 border-t text-xxs font-medium">Based in Yerevan. Open for creative collaborations.</p>
         </div>
-        <div class="pl-6 space-y-2">
-          <p class="text-xxs normal-case opacity-70">
+        <div class="pl-6">
+          <p class="mb-2 text-xxs normal-case opacity-70">
             Photography is how I capture the beauty of the world around us. I’m just here to share that vision through
             my lens. Let’s create something together
           </p>
-          <DefaultButton label="Get Started" size="mobileDark" />
+          <DefaultButton class="mb-6" label="Get Started" size="mobileDark" />
+          <img :src="promoSectionImg2" alt="" />
         </div>
       </div>
-    </div>
-    <div class="grid grid-cols-2">
-      <div class="pl-4 place-self-center text-sm">
-        <p>BEAUTY IS IN THE EYE OF&nbsp;THE BEHOLDER</p>
-      </div>
-      <img class="place-self-end w-5/6" :src="promoSectionImg2" alt="" />
     </div>
   </section>
 </template>
